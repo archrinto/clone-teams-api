bb11f82ef2b43d70156483c5206fc9f12883d872
July Arifianto <hey.archrinto@gmail.com>
2023-05-30 22:19:33 +0700

    add email conflix handler

--- src/users/actions/signup.js
+++ src/users/actions/signup.js
@@ -3,6 +3,10 @@ import User from "../../models/User.js";
 import Response from "../../utils/Response.js";
 
 export default async (req, res) => {
+    const existingUser = await User.findOne({email: req.body?.email });
+    
+    if (existingUser) return Response.confict(res, 'Email Already in Use')
+
     const user = await User.create({
         name: req.body.name,
         email: req.body.email,
