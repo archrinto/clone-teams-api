d2cdec0d691e1af62091b7177a52a9ca8e7b4cd1
July Arifianto <hey.archrinto@gmail.com>
2023-06-14 16:20:51 +0700

    add socket user media change event

--- src/socket.js
+++ src/socket.js
@@ -15,6 +15,7 @@ export default (server) => {
             return next(new Error("Unauthorized"));
         }
         socket.userId = user.id;
+        socket.user = user;
 
         return next();
     });
@@ -31,7 +32,8 @@ export default (server) => {
             console.log('user join a meeting', roomId, socket.userId);
             socket.join(roomId);
             socket.to(roomId).emit('user-join-meeting', {
-                userId: socket.userId
+                userId: socket.userId,
+                user: socket.user
             });
         });
 
@@ -47,7 +49,8 @@ export default (server) => {
             console.log('user send peer signal to', targetId, 'from', socket.userId);
             socket.to(targetId).emit('user-send-peer-signal', {
                 userId: socket.userId,
-                signal: signal
+                signal: signal,
+                user: socket.user
             })
         });
 
@@ -56,6 +59,14 @@ export default (server) => {
             socket.to(targetId).emit('user-return-peer-signal', {
                 userId: socket.userId,
                 signal: signal
+            })
+        })
+
+        socket.on('media-state-change', ({ roomId, mediaState }) => {
+            console.log('user media state change', roomId, socket.userId, mediaState);
+            socket.to(roomId).emit('user-media-state-change', {
+                userId: socket.userId,
+                mediaState: mediaState
             })
         })
 
