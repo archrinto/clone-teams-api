11368d2542985d5bc84ba027d4e0a1d255cdbba1
July Arifianto <hey.archrinto@gmail.com>
2023-06-15 21:52:38 +0700

    fix message order

--- src/chats/actions/getListMessage.js
+++ src/chats/actions/getListMessage.js
@@ -2,9 +2,11 @@ import Response from "../../utils/Response.js";
 import ChatMessage from "../../models/ChatMessage.js"
 
 export default async (req, res) => {
-    const limit = req.query?.limit || 30;
+    const limit = 30;
 
-    const messages = await ChatMessage.find({ 'chat._id': req.params.chatId }).limit(limit);
+    const messages = await ChatMessage.find({ 'chat._id': req.params.chatId })
+                    .sort({ createdAt: 'desc' })
+                    .limit(limit);
 
     return Response.success(res, messages);
 }
