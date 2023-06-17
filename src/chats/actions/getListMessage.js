5af94f76e7638d86f0c6e12690c5e4ee5c446a36
July Arifianto <hey.archrinto@gmail.com>
2023-06-17 15:38:10 +0700

    message pagination

--- src/chats/actions/getListMessage.js
+++ src/chats/actions/getListMessage.js
@@ -2,11 +2,13 @@ import Response from "../../utils/Response.js";
 import ChatMessage from "../../models/ChatMessage.js"
 
 export default async (req, res) => {
-    const limit = 30;
+    const limit = req?.query?.limit || 10;
+    const skip = req.query?.skip || 0;
 
     const messages = await ChatMessage.find({ 'chat._id': req.params.chatId })
                     .sort({ createdAt: 'desc' })
-                    .limit(limit);
+                    .limit(limit)
+                    .skip(skip);
 
     return Response.success(res, messages);
 }
